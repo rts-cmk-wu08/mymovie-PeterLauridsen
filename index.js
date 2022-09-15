9ad859d65147dde649d161b8cf5e7e25399547df
Peter Lauridsen <peter-lauridsen@live.dk>
2022-09-15 08:53:42 +0200

    added styles and new js with details.html

---
+++ index.js
@@ -0,0 +1,25 @@
+fetch('https://api.themoviedb.org/3/movie/now_playing?api_key=f6cd4ea3a6cc09f282a877cc9c2daed4&language=en-US&page=1')
+.then(nowPlaying => nowPlaying.json())
+.then(nowPlay => {
+    console.log(nowPlay)
+
+    let body = document.querySelector("body")
+    let section = document.createElement("section")
+    
+    section.classList.add("sliding")
+    body.append(section)
+
+    nowPlay.results.forEach(nowPlaying => {
+            let div = document.createElement("div")
+            div.classList.add("cards")
+            div.innerHTML = `
+            <img src="https://image.tmdb.org/t/p/original${nowPlaying.backdrop_path}" alt="">
+            
+            `
+            section.append(div)
+
+
+    })
+
+
+})
